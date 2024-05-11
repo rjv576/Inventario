1bdb2e99093e126df6a02e2edb9c6d38bc204544
Ronal Juma Vasquez <64800264+rjv576@users.noreply.github.com>
2024-05-10 21:51:35 -0400

    Modificando el archivo de configuración de git

--- capstone/mainapp/models.py
+++ capstone/mainapp/models.py
@@ -9,18 +9,18 @@ class Producto(models.Model):
     id = models.AutoField(primary_key=True)  
     title = models.CharField(max_length=100) 
     descripcion = models.TextField()
-    image = models.ImageField(blank=True,null=True,verbose_name='Imagen',upload_to='products/')
+    image = models.ImageField(blank=True,null=True,verbose_name='Imagen',upload_to='media')
     public = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)  # Call the "real" save() method.
+def save(self, *args, **kwargs): # Redefinir el metodo save
+    super(Producto, self).save(*args, **kwargs) # Llamar al metodo save de la clase padre
 
-        img = Image.open(self.image.path)  # Open the image file.
-
-        if img.height > 300 or img.width > 300:  # If the image is larger than 300x300 pixels...
-            output_size = (300, 300)  # Set the new size.
-            img.thumbnail(output_size)  # Resize the image.
-            img.save(self.image.path)  # Save the image back to the same path.
+    if self.image: # Si la imagen existe
+        img = Image.open(self.image.path) # Abrir la imagen
+        if img.height > 300 or img.width > 300: # Si la imagen es mayor a 300px
+            output_size = (300, 300) # Tamaño de salida
+            img.thumbnail(output_size) # Redimensionar la imagen
+            img.save(self.image.path) # Guardar la imagen redimensionada
   
